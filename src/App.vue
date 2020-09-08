556d8af85fbb1d7ea62b0f88a5a56cd1a75ee2c5
vasyl.puhinskyy <vasil.puhinskiy@guidance.com>
2020-09-08 15:04:23 +0300

    Vue initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+    <Header />
+    <FirstScreen />
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header/Header'
+import FirstScreen from './components/MainOffer/MainOffer'
+export default {
+  name: 'App',
+  components: { Header, FirstScreen }
+}
+</script>
+
+<style lang="scss">
+  @import 'assets/scss/main.scss';
+</style>
