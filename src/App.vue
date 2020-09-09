573d1edcfd7168345ea2d1813d805fb14251cbd7
vasyl.puhinskyy <vasil.puhinskiy@guidance.com>
2020-09-09 16:08:37 +0300

    Implemented component - "Donation"

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <AboutUs />
     <OurFriends />
     <YouCanHelp />
+    <Donation />
   </div>
 </template>
 
@@ -13,9 +14,10 @@ import FirstScreen from './components/FirstScreen/FirstScreen'
 import AboutUs from './components/AboutUs/AboutUs'
 import OurFriends from './components/OurFriends/OurFriends'
 import YouCanHelp from './components/YouCanHelp/YouCanHelp'
+import Donation from './components/Donation/Donation'
 export default {
   name: 'App',
-  components: { YouCanHelp, OurFriends, AboutUs, FirstScreen }
+  components: { Donation, YouCanHelp, OurFriends, AboutUs, FirstScreen }
 }
 </script>
 
---
+++ src/components/Donation/Donation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="donation-wrap">
+    <div class="donation block-wrap">
+      <img class="donation-picture" src="../../assets/img/donation.jpg" alt="">
+      <div class="donation-content">
+        <h3 class="block-title">
+          In addition, you <br>
+          can make a donation
+        </h3>
+        <strong class="bank-info">Name of the bank / Type of bank account</strong>
+        <div class="bank-card">
+          <Icon
+            :icon="'credit-card'"
+            :tittle="'Credit card'"
+          />
+          <span class="credit-card-number">
+            8380 2880 8028 8791 7435
+          </span>
+        </div>
+        <p class="legal-information">Legal information and lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Maecenas a ipsum at libero sagittis dignissim sed ac diam. Praesent ultrices maximus tortor et vulputate.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '../Icon/Icon'
+export default {
+  name: 'Donation',
+  components: { Icon }
+}
+</script>
+
+<style scoped lang="scss">
+  @import 'donation';
+</style>
