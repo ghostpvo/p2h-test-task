d3c60cbf63e1c0d670a234690ab69f64926b5e7d
vasyl.puhinskyy <vasil.puhinskiy@guidance.com>
2020-09-09 12:13:16 +0300

    Icons added

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg
+    :class="iconId"
+    class="icon"
+    viewBox="0 0 100 100"
+  >
+    <!-- TODO '~' - не работал в обратных кавычках, временно поставил '/_nuxt/', надо сделать норм -->
+    <use :xlink:href="getSprite(iconId)">
+      {{ tittle }}
+    </use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    tittle: {
+      type: String,
+      default: ''
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    iconId () {
+      if (this.isActive) {
+        return this.icon + '-active'
+      } else {
+        return this.icon
+      }
+    }
+  },
+  methods: {
+    getSprite (type) {
+      return require(`~/static/sprite/sprite.svg`) + '#' + type
+    }
+  }
+}
+</script>
